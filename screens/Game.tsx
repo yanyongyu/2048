b5652ac86f1f857d2c88cc4eb87ccf068d6065eb
yanyongyu <yanyongyu_1@126.com>
2021-10-13 15:50:26 +0800

    :construction: process game content

--- screens/Game.tsx
+++ screens/Game.tsx
@@ -6,6 +6,22 @@ import { StyleSheet, Text, View } from "react-native";
 import { Container } from "../components/Container";
 import { GameGrid } from "../components/GameGrid";
 
+function ScoreView({
+  title,
+  score,
+}: {
+  title: String;
+  score: Number;
+}): JSX.Element {
+  return (
+    <View style={styles.scoreWrapper}>
+      <View style={styles.scoreContainer}>
+        <Text>{title}</Text>
+      </View>
+    </View>
+  );
+}
+
 export default function Game({
   navigation,
   route,
@@ -13,8 +29,17 @@ export default function Game({
   const { type } = route.params;
   return (
     <Container>
-      <View>
-        <Text>2048</Text>
+      <View style={styles.heading}>
+        <View style={styles.headingItem}>
+          <Text style={styles.headingText}>2048</Text>
+        </View>
+        <View style={styles.headingItem}>
+          <View style={styles.scoreBar}>
+            <ScoreView title="Score" score={0} />
+            <ScoreView title="Best" score={0} />
+          </View>
+          <View style={styles.toolBar}></View>
+        </View>
       </View>
       <View>
         <GameGrid type={type as typeof TYPES[number]} />
@@ -23,4 +48,37 @@ export default function Game({
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    width: "100%",
+    flexDirection: "row",
+  },
+  headingItem: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headingText: {
+    textAlign: "center",
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "#776e65",
+  },
+
+  scoreBar: {
+    flex: 2,
+    flexDirection: "row",
+  },
+  scoreWrapper: {
+    flex: 1,
+  },
+  scoreContainer: {
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+    padding: 10,
+  },
+
+  toolBar: {
+    flex: 1,
+    flexDirection: "row",
+  },
+});
